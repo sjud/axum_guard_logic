335baf0ce310e2f6804c41535a726d615ff73b7d
sjud <64875465+sjud@users.noreply.github.com>
2022-09-01 11:15:24 -0400

    rewrite finished

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,85 @@
+use std::convert::Infallible;
 use std::task::{Poll, Context};
 use std::future::Future;
 use std::marker::{PhantomData, PhantomPinned};
 use std::pin::{Pin};
 use std::sync::{Arc, Mutex};
+use axum_core::body::BoxBody;
 use axum_core::extract::{FromRequestParts};
-use axum_core::response::IntoResponse;
+use axum_core::response::{IntoResponse, Response};
 use futures_core::future::BoxFuture;
 use tower_service::Service;
 use http::{Request, StatusCode};
 use http::request::Parts;
+use axum_core::BoxError;
+use futures_core::ready;
 use tower_layer::Layer;
 
 pub trait Guard {
     fn check_guard(&self, expected:&Self) -> bool;
 }
 
-pub struct GuardLayer<GuardService>
+pub trait GuardServiceExt : Service<Parts, Response=GuardServiceResponse,Error=StatusCode> + Clone + Send {
+    fn or<Other>(self,other:Other)
+        -> OrGuardService<Self,Other>
+        where
+            Other:Service<Parts, Response=GuardServiceResponse,Error=StatusCode> + Clone + Send,
+            <Other as Service<Parts>>::Future: Send,
+            <Self as Service<Parts>>::Future: Send {
+        OrGuardService{
+            left:self,
+            right: other,
+        }
+    }
+    fn and<Other>(self,other:Other)
+        -> AndGuardService<Self,Other>
+        where
+            Other:Service<Parts, Response=GuardServiceResponse,Error=StatusCode> + Clone + Send,
+            <Other as Service<Parts>>::Future: Send,
+            <Self as Service<Parts>>::Future: Send {
+        AndGuardService{
+            left:self,
+            right: other,
+        }
+    }
+    fn into_layer<ReqBody>(self) -> GuardLayer<Self,ReqBody> {
+        GuardLayer::with(self)
+    }
+}
+impl<T> GuardServiceExt for T
+    where
+        T: Service<Parts, Response=GuardServiceResponse,Error=StatusCode> + Clone + Send {}
+
+pub struct GuardLayer<GuardService,ReqBody>
     where
     GuardService:Service<Parts, Response=GuardServiceResponse,Error=StatusCode>
     + Send + Clone + 'static {
     guard_service:GuardService,
+    _marker:PhantomData<ReqBody>
 }
 
-impl<GuardService> GuardLayer<GuardService>
+impl<GuardService,ReqBody> Clone for GuardLayer<GuardService,ReqBody>
     where
-        GuardService:Service<Parts, Response=GuardServiceResponse,Error=StatusCode>{
-    pub fn with(guard:GuardService) -> Self {
-        Self{ guard_service:guard }
+        GuardService:Service<Parts, Response=GuardServiceResponse,Error=StatusCode>
+        + Send + Clone + 'static {
+    fn clone(&self) -> Self {
+        Self{
+            guard_service: self.guard_service.clone(),
+            _marker: PhantomData,
+        }
     }
 }
-impl<S,GuardService,ReqBody> Layer<S> for GuardLayer<GuardService>
+impl<GuardService,ReqBody> GuardLayer<GuardService,ReqBody>
     where
-        S:Service<Request<ReqBody>>,
+        GuardService:Service<Parts, Response=GuardServiceResponse,Error=StatusCode>
+        + Send + Clone + 'static {
+    pub fn with(guard:GuardService) -> Self {
+        Self{ guard_service:guard, _marker:PhantomData }
+    }
+}
+impl<S,GuardService,ReqBody> Layer<S> for GuardLayer<GuardService,ReqBody>
+    where
+        S:Service<Request<ReqBody>> + Clone,
         GuardService:Service<Parts, Response=GuardServiceResponse,Error=StatusCode>
         + Send + Clone + 'static {
     type Service = GuardServiceWrapper<S,GuardService,ReqBody>;
@@ -40,49 +88,114 @@ impl<S,GuardService,ReqBody> Layer<S> for GuardLayer<GuardService>
         GuardServiceWrapper{
             inner,
             guard_service:self.guard_service.clone(),
+            _marker:PhantomData
         }
     }
 }
 pub struct GuardServiceWrapper<S,GuardService,ReqBody>
     where
-        S:Service<Request<ReqBody>>,
-        GuardService:Service<Parts, Response=GuardServiceResponse,Error=StatusCode>
+        S:Service<Request<ReqBody>> + Clone,
+        GuardService:Service<Parts, Response=GuardServiceResponse>
         + Send + Clone + 'static {
     inner:S,
     guard_service:GuardService,
+    _marker:PhantomData<ReqBody>
 }
-impl<S,ReqBody,GuardService,Response> Service<Request<ReqBody>> for
+impl<S,GuardService,ReqBody> Clone for GuardServiceWrapper<S,GuardService,ReqBody>
+    where
+        S:Service<Request<ReqBody>> + Clone,
+        GuardService:Service<Parts, Response=GuardServiceResponse>
+        + Send + Clone + 'static {
+    fn clone(&self) -> Self {
+        Self{
+            inner: self.inner.clone(),
+            guard_service: self.guard_service.clone(),
+            _marker: PhantomData
+        }
+    }
+}
+impl<S,ReqBody,GuardService> Service<Request<ReqBody>> for
 GuardServiceWrapper<S,GuardService,ReqBody>
     where
-        Response:IntoResponse,
-        S:Service<Request<ReqBody>>,
-        GuardService:Service<Parts, Response=GuardServiceResponse,Error=StatusCode>
+        ReqBody:Send +'static,
+        S:Service<Request<ReqBody>, Response = Response, Error = Infallible> + Send + Clone + 'static,
+        <S as Service<Request<ReqBody>>>::Future: Send,
+        <GuardService as Service<Parts>>::Future: Send,
+        GuardService:Service<Parts, Response=GuardServiceResponse, Error = StatusCode>
         + Send + Clone + 'static{
-    type Response = Result<S::Response,S::Error>;
-    type Error = StatusCode;
-    type Future = BoxFuture<'static, Result<Self::Response,Self::Error>> ;
+    type Response = Response;
+    type Error = Infallible;
+    type Future = BoxFuture<'static,Result<Response,Infallible>>;
 
     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        self.inner.poll_ready(cx)
+        Poll::Ready(Ok(()))
     }
 
     fn call(&mut self, req: Request<ReqBody>) -> Self::Future {
-        Box::pin(
-            async move {
-                let (parts,body) = req.into_parts();
-                let GuardServiceResponse(result,parts) = self.guard_service.call(parts)
-                    .await?;
-                if result {
-                    self.inner.call(Request::from_parts(parts,body))
-                } else {
-                    Err(StatusCode::UNAUTHORIZED)
+        let (parts,body) = req.into_parts();
+        let f = self.guard_service.call(parts);
+        let mut inner = self.inner.clone();
+        Box::pin(async move {
+            match f.await {
+                Ok(GuardServiceResponse(result,parts)) => {
+                    if result {
+                        inner.call(Request::from_parts(parts,body))
+                            .await
+                    } else {
+                        Ok(StatusCode::UNAUTHORIZED.into_response())
+                    }
+                },
+                Err(status) => {
+                    Ok(status.into_response())
                 }
             }
-        )
-
+        })
     }
 }
+/*
+This panics when the inner future returns pending... how to fix?
+#[pin_project::pin_project]
+pub struct GuardFuture<S,ReqBody,GuardService>
+    where
+        ReqBody:Send,
+        S:Service<Request<ReqBody>, Response = Response, Error = Infallible> + Send + Clone,
+        <S as Service<Request<ReqBody>>>::Future:Unpin,
+        GuardService:Service<Parts, Response=GuardServiceResponse>
+        + Send + Clone + 'static, {
+    #[pin]
+    f:<GuardService as Service<Parts>>::Future,
+    inner:S,
+    body:ReqBody
+}
 
+impl<S,ReqBody,GuardService> Future for GuardFuture<S,ReqBody,GuardService>
+    where
+        ReqBody:Send,
+        S:Service<Request<ReqBody>, Response = Response, Error = Infallible> + Send + Clone,
+        <S as Service<Request<ReqBody>>>::Future: Unpin,
+        GuardService:Service<Parts, Response=GuardServiceResponse, Error = StatusCode>
+        + Send + Clone + 'static, {
+    type Output = Result<Response,Infallible>;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = self.project();
+        match ready!(this.f.poll(cx)) {
+            Ok(GuardServiceResponse(result,parts)) => {
+                if result {
+                    Pin::new(&mut this.inner
+                        .call(Request::from_parts(parts,this.body.take().unwrap())))
+                        .poll(cx)
+                } else {
+                    Poll::Ready(Ok(StatusCode::UNAUTHORIZED.into_response()))
+                }
+            },
+            Err(status) => {
+                Poll::Ready(Ok(status.into_response()))
+            }
+        }
+    }
+}
+*/
 impl<State,G> GuardService< State,G>
     where
         State:Clone,
@@ -275,7 +388,9 @@ pub mod tests {
             })
         }
     }
-
+    async fn ok() -> StatusCode {
+        StatusCode::OK
+    }
     use super::*;
 
     #[tokio::test]
@@ -409,4 +524,227 @@ pub mod tests {
             ).call(parts).await.unwrap().0
         )
     }
+    #[tokio::test]
+    async fn test_layer() {
+        let req = Request::builder()
+            .header("data","data")
+            .body(BoxBody::default())
+            .unwrap();
+
+        let data = ArbitraryData { data: "data".into() };
+        let app = Router::new()
+            .route("/", get(ok))
+            .layer(
+                GuardLayer::with(
+                    GuardService::new(
+                        data.clone(),
+                        data.clone()))
+            );
+        let resp = app.oneshot(req).await.unwrap();
+        assert_eq!(resp.status(),StatusCode::OK)
+    }
+    #[tokio::test]
+    async fn test_layer_or_ok() {
+        let req = Request::builder()
+            .header("data","data")
+            .header("not_data","wazzup i'm a criminal")
+            .body(BoxBody::default())
+            .unwrap();
+
+        let data = ArbitraryData { data: "data".into() };
+        let app = Router::new()
+            .route("/", get(ok))
+            .layer(
+                GuardLayer::with(
+                    GuardService::new(
+                        data.clone(),
+                        data.clone())
+                        .or(GuardService::new(
+                            ArbitraryData{data:"not_data".into()},
+                            data,
+                        ))
+                )
+            );
+        let resp = app.oneshot(req).await.unwrap();
+        assert_eq!(resp.status(),StatusCode::OK)
+    }
+    #[tokio::test]
+    async fn test_layer_or_not_ok() {
+        let req = Request::builder()
+            .header("data","wazzup i'm a criminal")
+            .header("not_data","wazzup i'm a criminal")
+            .body(BoxBody::default())
+            .unwrap();
+
+        let data = ArbitraryData { data: "data".into() };
+        let app = Router::new()
+            .route("/", get(ok))
+            .layer(
+                GuardLayer::with(
+                    GuardService::new(
+                        data.clone(),
+                        data.clone())
+                        .or(GuardService::new(
+                            ArbitraryData{data:"not_data".into()},
+                            data,
+                        ))
+                )
+            );
+        let resp = app.oneshot(req).await.unwrap();
+        assert_eq!(resp.status(),StatusCode::UNAUTHORIZED)
+    }
+    #[tokio::test]
+    async fn test_layer_and_ok() {
+        let req = Request::builder()
+            .header("data","data")
+            .header("not_data","not_data")
+            .body(BoxBody::default())
+            .unwrap();
+
+        let data = ArbitraryData { data: "data".into() };
+        let definitely_still_data = ArbitraryData{data:"not_data".into()};
+        let app = Router::new()
+            .route("/", get(ok))
+            .layer(
+                GuardLayer::with(
+                    GuardService::new(
+                        data.clone(),
+                        data.clone())
+                        .and(GuardService::new(
+                            definitely_still_data.clone(),
+                            definitely_still_data,
+                        ))
+                )
+            );
+        let resp = app.oneshot(req).await.unwrap();
+        assert_eq!(resp.status(),StatusCode::OK)
+    }
+    #[tokio::test]
+    async fn test_layer_and_not_ok() {
+        let req = Request::builder()
+            .header("data","data")
+            .header("not_data","GRRR I SO HACK U")
+            .body(BoxBody::default())
+            .unwrap();
+
+        let data = ArbitraryData { data: "data".into() };
+        let definitely_still_data = ArbitraryData{data:"not_data".into()};
+        let app = Router::new()
+            .route("/", get(ok))
+            .layer(
+                GuardLayer::with(
+                    GuardService::new(
+                        data.clone(),
+                        data.clone())
+                        .and(GuardService::new(
+                            definitely_still_data.clone(),
+                            definitely_still_data,
+                        ))
+                )
+            );
+        let resp = app.oneshot(req).await.unwrap();
+        assert_eq!(resp.status(),StatusCode::UNAUTHORIZED)
+    }
+
+    #[tokio::test]
+    async fn test_deep_layer_ok() {
+        let req = Request::builder()
+            .header("1","1")
+            .header("2","2")
+            .header("3","3")
+            .header("4","4")
+            .header("5","5")
+            .header("6","6")
+            .header("7","8")
+            .body(BoxBody::default())
+            .unwrap();
+
+        let one = ArbitraryData { data: "1".into() };
+        let two = ArbitraryData { data: "2".into() };
+        let three = ArbitraryData { data: "3".into() };
+        let four = ArbitraryData { data: "4".into() };
+        let five = ArbitraryData { data: "5".into() };
+        let six = ArbitraryData { data: "6".into() };
+        let seven = ArbitraryData { data: "7".into() };
+        let bad = ArbitraryData { data: "bad".into() };
+
+        let app = Router::new()
+            .route("/", get(ok))
+            .layer(
+                GuardLayer::with(
+                    GuardService::new(
+                        one.clone(),
+                        one.clone()).and(
+                        GuardService::new(
+                            two.clone(),
+                            bad.clone()
+                        ).or(
+                            GuardService::new(
+                                three.clone(),
+                                bad.clone()
+                            )
+                                .or(
+                                    GuardService::new(
+                                        four.clone(),
+                                        four.clone(),
+                                    )
+                                )
+                        )
+                    ).and(
+                        GuardService::new(five.clone(), five.clone()
+                        )
+                    ).and(
+                        GuardService::new(six.clone(),six.clone())
+                    )
+                        .or(GuardService::new(seven.clone(),bad.clone())))
+            );
+        let resp = app.oneshot(req).await.unwrap();
+        assert_eq!(resp.status(),StatusCode::OK)
+    }
+    async fn time_time<B>(req: Request<B>, next: Next<B>) -> Result<Response, StatusCode> {
+        sleep(Duration::from_millis(10)).await;
+        Ok(next.run(req).await)
+    }
+    #[tokio::test]
+    async fn test_into_layer() {
+        let req = Request::builder()
+            .header("data","data")
+            .body(BoxBody::default())
+            .unwrap();
+
+        let data = ArbitraryData { data: "data".into() };
+        let app = Router::new()
+            .route("/", get(ok))
+            .layer(
+                    GuardService::new(data.clone(), data.clone())
+                        .into_layer()
+            );
+        let resp = app.oneshot(req).await.unwrap();
+        assert_eq!(resp.status(),StatusCode::OK)
+    }
+    #[tokio::test]
+    async fn test_happy_with_layered_polls() {
+        let app = Router::new()
+            .route("/",get(ok))
+            .layer(axum::middleware::from_fn(time_time))
+            .layer(GuardLayer::with(
+                GuardService::new(
+                    ArbitraryData{data:"x".into()},ArbitraryData{data:"x".into()}
+                )))
+            .layer(axum::middleware::from_fn(time_time))
+            .layer(tower_http::timeout::TimeoutLayer::new(Duration::from_secs(1)));
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .header("x","x")
+                    .uri("/")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+    }
+
 }
